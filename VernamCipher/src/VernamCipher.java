293939a85c86dd2c10009aa4590580af10105ba4
diellakika <diella.kika@student.uni-pr.edu>
2025-04-18 22:11:26 +0200

    -

--- VernamCipher/src/VernamCipher.java
+++ VernamCipher/src/VernamCipher.java
@@ -8,7 +8,7 @@ public class VernamCipher {
         }
         return cipherText.toString();
     }
-   
+
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         System.out.println("Enter plaintext:");
